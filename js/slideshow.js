2ab65e12f0a9f05feaf619914cd0832496c48a7e
Jing Ma <jingma@bu.edu>
2025-01-26 18:49:32 -0600

    refactor slideshow

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,28 +1,40 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
+let num_instance = 1;
+showSlides();
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  slideIndex += n - 1;
+  num_instance += 1;
+  showSlides();
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  slideIndex = n - 1;
+  num_instance += 1;
+  showSlides();
 }
 
-function showSlides(n) {
+function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  slideIndex = (slideIndex + slides.length) % slides.length + 1;
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" slideshow-active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " slideshow-active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " slideshow-active";
+
+  if (num_instance == 1) {
+    setTimeout(showSlides, 5000); // change image every 5 seconds
+  }
+  else {
+    num_instance -= 1;
+  }
 } 
