695b8ce96c88b45ca879b627c8e783992e781130
Jing Ma <jingma@bu.edu>
2025-11-17 12:35:32 -0500

    add group pictures, refactor people page

--- people_page_gen.py
+++ people_page_gen.py
@@ -247,7 +247,7 @@ people_html += current_members_html
 
 ################ group pictures ################
 
-group_pitures_html = """
+group_pictures_html = """
         <div class="col-sm-12">
           <div class="service-box">
             <div class="row">
@@ -268,22 +268,16 @@ group_pitures_html = """
               </div>
 """
 
-group_pitures = []
-people_filename = "data/people.csv"
-with open(people_filename, 'r') as people_file:
-    people_reader = csv.DictReader(people_file)
-    for person in people_reader:
-        if person["display"] == "true" and person["category"] == "group":
-            group_pitures.append(person)
+group_pictures = people["group_pictures"]
 
-for group_piture in group_pitures:
-    group_pitures_html += """
+for group_picture in group_pictures:
+    group_pictures_html += """
               <div class="col-md-6">
                 <div class="row">
                   <div class="col-sm-1 col-md-1"></div>
                   <div class="col-sm-10 col-md-10">
                     <div class="about-img">
-                      <img src="img/people/{}" class="img-fluid rounded b-shadow-a" alt="">
+                      <img src="img/group/{}" class="img-fluid rounded b-shadow-a" alt="">
                     </div>
                   </div>
                   <div class="col-sm-1 col-md-1"></div>
@@ -298,124 +292,17 @@ for group_piture in group_pitures:
                   <div class="col-sm-1 col-md-1"></div>
                 </div>
               </div>
-  """.format(group_piture["img"], group_piture["name"])
+  """.format(group_picture["picture"], group_picture["name"])
 
-group_pitures_html += """
+group_pictures_html += """
             </div>
           </div>
         </div>
 """
-people_html += group_pitures_html
-
-#################### alumni ####################
-
-alumni_html = """
-        <div class="col-sm-12">
-          <div class="service-box">
-            <div class="row">
-              <div class="col-md-12">
-                <div class="row">
-                  <wide-padding>
-                    <div id = "alumni">
-                      <div class="service-content">
-                        <h2 class="s-title text-center">
-                          <p class = "text-left">
-                            Alumni
-                          </p>
-                        </h2>
-                      </div>
-                    </div>
-                  </wide-padding>
-                </div>
-              </div>
-"""
-
-alumni = []
-people_filename = "data/people.csv"
-with open(people_filename, 'r') as people_file:
-    people_reader = csv.DictReader(people_file)
-    for person in people_reader:
-        if person["display"] == "true" and person["category"] == "alumnus":
-            alumni.append(person)
-
-for alumnus in alumni:
-    alumni_html += """
-              <div class="col-md-3">
-                <div class="row">
-                  <div class="col-sm-1 col-md-1"></div>
-                  <div class="col-sm-10 col-md-10">
-                    <div class="about-img">
-                      <img src="img/people/{}" class="img-fluid rounded b-shadow-a" alt="">
-                    </div>
-                  </div>
-                  <div class="col-sm-1 col-md-1"></div>
-                </div>
-                <div class="row">
-                  <div class="col-sm-1 col-md-1"></div>
-                  <div class="col-sm-10 col-md-10">
-                    <div class="text-center" style="margin-top: 5px; margin-bottom: 15px;">
-                      {}
-                    </div>
-                  </div>
-                  <div class="col-sm-1 col-md-1"></div>
-                </div>
-              </div>
-  """.format(alumnus["img"], alumnus["name"])
-
-alumni_html += """
-            </div>
-          </div>
-        </div>
-"""
-# people_html += alumni_html
+people_html += group_pictures_html
 
 ################ collaborators #################
 
-################# first layout #################
-collaborators_html = """
-        <div class="col-sm-12">
-          <div class="service-box">
-            <div class="row">
-              <div class="col-md-12">
-                <div class="row">
-                  <wide-padding>
-                    <div id = "collaborators">
-                      <div class="service-content">
-                        <h2 class="s-title text-center">
-                          <p class = "text-left">
-                            Key collaborators
-                          </p>
-                        </h2>
-"""
-
-collaborators = []
-collaborators_filename = "data/collaborators.csv"
-with open(collaborators_filename) as collaborators_file:
-    collaborators_reader = csv.reader(collaborators_file)
-    for collaborator in collaborators_reader:
-        collaborators.append(collaborator)
-collaborators.sort(key=lambda c: c[0].split()[-1])
-
-for name, affiliation in collaborators:
-    collaborators_html += """
-                        <p>
-                          {}, {}
-                        </p>
-  """.format(name, affiliation)
-
-collaborators_html += """
-                      </div>
-                    </div>
-                  </wide-padding>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-"""
-# people_html += collaborators_html
-
-################ second layout #################
 collaborators_html = """
         <div class="col-sm-12">
           <div class="service-box">
@@ -437,13 +324,12 @@ collaborators_html = """
               </div>
 """
 
-collaborators = ["bar-ilan.png", "bu.png", "icl.jpg",
-                 "kth.png", "nordita.png", "neu.png", "nwu.png", "oist.jpg", "oxford.jpg"]
+collaborators = people["collaborators"]
 
 for collaborator in collaborators:
     collaborators_html += """
               <img src="img/logos/{}" style="width: 18%; height: 18%; margin: auto;">
-  """.format(collaborator)
+  """.format(collaborator["picture"])
 
 collaborators_html += """
             </div>
